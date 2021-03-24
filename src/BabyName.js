9b4c1b0bb1adf9e8deef79671bc5a91a11edbe90
Michael <antenehnigussie1@yahoo.com>
2021-03-24 14:20:47 +0000

    Removed unnecessary comment line.

--- src/BabyName.js
+++ src/BabyName.js
@@ -1,10 +1,13 @@
-import React from "react";
-
 const BabyName = (props) => {
-    // console.log(props);
   return (
-    <button id={props.id} className={`name ${props.sex}-name`}>{props.name}</button>
+    <button
+      id={props.id}
+      className={`name ${props.sex}-name`}
+      onDoubleClick={props.onDoubleClick}
+    >
+      {props.name}
+    </button>
   );
-}
+};
 
 export default BabyName;
